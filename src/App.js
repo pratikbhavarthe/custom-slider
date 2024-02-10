9d48e206224c94d79a856f1ac354f36bb77c69f1
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-02-10 20:19:31 +0530

    setting up project structure ⚙️

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import CustomSlider from "./Components/CustomSlider";
+import React, { useState, useEffects } from "react";
 function App() {
+  const [track, setTrack] = useState(5);
+  const [min, setMin] = useState(Number(-5));
+  const [max, setMax] = useState(55);
+  const [min2, setMin2] = useState(Number(-5));
+  const [max2, setMax2] = useState(55);
+  const [sliderValue, setSliderValue] = useState(0);
+  useEffect(() => {
+    setMin(Math.round(Math.random() * 5) - 10);
+    setMax(Math.round(Math.random() * 150) + 50);
+
+    setMin2(Math.round(Math.random() * 5));
+    setMax2(Math.round(Math.random() * 150) + 25);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="control-panel-container">
+        <div className="hypen"/>
+        <div className="control-panel">
+          <div className="control-panel__item">
+            <label>Track</label>
+            <input value={track} type="number" onChange={(e) => setTrack(e.target.value)}/>
+          </div>
+          <div className="control-panel__item">
+            <label>Minimum</label>
+            <input type="number" value={max} onChange={(e) => setMin(e.target.value)}/>
+          </div>
+          <div className="control-panel__item">
+            <label>Maximum</label>
+            <input type="number" value={max} onChange={(e) => setMax(e.target.value)}/>
+          </div>
+        </div>
+        <div className="hyphen"/>
+      </div>
+      
     </div>
-  );
+  )
 }
-
-export default App;
